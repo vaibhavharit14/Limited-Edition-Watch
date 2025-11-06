26b9b94ca0d28c8e520a7babcb07431c8810df13
vaibhav harit <v.harit14@gmail.com>
2025-11-06 17:05:28 +0530

    feat:update app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Limited-Edition-Watch"> {/* ✅ Added basename */}
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
